bbbaf77892ec9f71d4c497f274f20eac489e2cd8
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-03 14:47:47 +0200

    Added circular movement switch and script

---
+++ Assets/Scripts/CircularMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+    [SerializeField] public float radius = 2f;
+    [SerializeField] public float speed = 2f;
+    [SerializeField] public bool StartCircularMovement = false;
+
+    private float angle = 0f;
+    private Vector3 centerPoint;
+
+    private void Start()
+    {
+        centerPoint = transform.position;
+    }
+
+    private void Update()
+    {
+        if (StartCircularMovement) 
+        {
+            angle += speed * Time.deltaTime;
+            float x = centerPoint.x + radius * Mathf.Cos(angle);
+            float y = centerPoint.y + radius * Mathf.Sin(angle);
+            float z = centerPoint.z;
+
+            transform.position = new Vector3(x, y, z);
+        }
+    }
+}
