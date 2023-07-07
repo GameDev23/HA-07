707b66e0f4be56931797f845ed89ce9f0e970303
HerrKermet <david.stechow1113@gmail.com>
2023-07-07 13:10:09 +0200

    adjusted script

--- Assets/Scripts/SlowReadjust.cs
+++ Assets/Scripts/SlowReadjust.cs
@@ -6,35 +6,26 @@ public class SlowReadjust : MonoBehaviour
 {
     public Transform target;
     public float duration = 3f;
+    public float Speed = 1f;
 
     private Vector3 startPosition;
     private float elapsedTime = 0f;
 
+    private bool reachedDestination = false;
+
     private void Start()
     {
         startPosition = transform.position;
-        target.position = new Vector3(11.4f, -1.5f, -54.32846f);
     }
 
     private void Update()
     {
-        /*
-        // Check if we've reached the target position
-        if (elapsedTime >= duration)
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * Speed);
+
+        if (!reachedDestination && Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
+            reachedDestination = true;
             transform.position = target.position;
-            return;
         }
-
-        // Calculate the interpolation factor (0 to 1)
-        float t = elapsedTime / duration;
-
-        // Smoothly move the object
-        // transform.position = Vector3.Lerp(startPosition, target.position, t);
-        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime);
-
-        // Increase the elapsed time
-        elapsedTime += Time.deltaTime;
-        */
     }
 }
